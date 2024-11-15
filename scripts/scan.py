00f68703227c82453e63bb1b20b22f4de9dd7070
nathanMils <106823768+nathanMils@users.noreply.github.com>
2024-11-15 15:25:06 +0200

    Check

--- scripts/scan.py
+++ scripts/scan.py
@@ -17,6 +17,9 @@ def main():
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
     
+    # extension_path = '/home/nathan/Desktop/781-Project/local-chrome-extension'
+    # chrome_options.add_argument(f'--load-extension={extension_path}')
+
     driver_path = '/home/nathan/Desktop/chromedriver-linux64/chromedriver'
 
     chrome_options.binary_location = "/usr/bin/google-chrome"
@@ -24,27 +27,11 @@ def main():
     service = Service(driver_path)
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
-    def is_url_online(url):
-        """
-        Checks if the URL is reachable and returns True if the status is 200 OK.
-        """
-        try:
-            # Use 'requests.head' to only check the status of the URL without downloading the entire content
-            response = requests.head(url, allow_redirects=True, timeout=1)
-            return response.status_code == 200
-        except requests.RequestException:
-            return False
-
     def collect_website_info(url):
         try:
-            # Use Selenium to get the URL and process the page
             driver.get(url)
-            WebDriverWait(driver, 4).until(EC.presence_of_element_located((By.TAG_NAME, 'div')))
 
-            # Check if the redirected URL is valid
-            if not is_url_online(url):
-                print(f"Skipping {url} as it's no longer reachable.")
-                return None
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'a')))
 
             page_title = driver.title
             page_source = driver.page_source
@@ -60,27 +47,22 @@ def main():
             }
 
         except Exception as e:
-            print(f"Error processing {url}: {e}")
             return None
 
-    # Load the list of URLs
     df = pd.read_csv('./data/phishtank/verified_online.csv')
     urls = df['url'].iloc[550:10000].tolist()
 
     collected_data = []
 
-    # Iterate over the URLs and collect information
     for url in tqdm(urls, desc="Processing URLs", ncols=100):
         info = collect_website_info(url)
         if info:
             collected_data.append(info)
 
-    # Save collected data to a CSV file
     if collected_data:
         collected_df = pd.DataFrame(collected_data)
         collected_df.to_csv('collected_website_info.csv', index=False)
 
-    # Close the Selenium WebDriver
     driver.quit()
 
 if __name__ == "__main__":
